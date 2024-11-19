75a14d13e0007c5265eb64aabf502cce8ccf1774
V-SANT <vsantander@fi.uba.ar>
2024-11-19 20:13:58 -0300

    Verification Photo View

--- src/components/VerificationView.js
+++ src/components/VerificationView.js
@@ -1,12 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import { getAllUsers, getProfileByUsername } from '../handlers/ProfileHandler'; 
+import { getAllUsers, getProfileByUsername } from '../handlers/ProfileHandler';
 import '../styles/VerificationView.css';
-import UserDetailsModal from './UserDetailsModal'; 
 
-// Caché para almacenar los perfiles de usuario temporalmente
 let profilesCache = null;
 
-// Componente para el indicador de estado
 const StatusIndicator = ({ isVerified }) => (
   <span
     style={{
@@ -14,7 +11,7 @@ const StatusIndicator = ({ isVerified }) => (
       width: '10px',
       height: '10px',
       borderRadius: '50%',
-      backgroundColor: isVerified ? '#4CAF50' : '#F44336', // Verde si es verificado, rojo si no
+      backgroundColor: isVerified ? '#4CAF50' : '#F44336',
       marginRight: '5px',
     }}
   />
@@ -22,29 +19,33 @@ const StatusIndicator = ({ isVerified }) => (
 
 const VerificationView = () => {
   const [profiles, setProfiles] = useState([]);
-  const [loading, setLoading] = useState(true); // Estado de carga general
+  const [filteredProfiles, setFilteredProfiles] = useState([]);
+  const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  const [selectedUser, setSelectedUser] = useState(null); 
-  const [isModalOpen, setIsModalOpen] = useState(false); 
+  const [expandedUser, setExpandedUser] = useState(null);
+  const [modalImage, setModalImage] = useState(null);
+
+  const [searchTerm, setSearchTerm] = useState('');
+  const [filterStatus, setFilterStatus] = useState('');
 
   const fetchProfiles = async () => {
-    setLoading(true); // Mostrar círculo de carga
+    setLoading(true);
     const usernames = await getAllUsers();
 
     if (profilesCache) {
       setProfiles(profilesCache);
-      setLoading(false); 
+      setFilteredProfiles(profilesCache);
+      setLoading(false);
       return;
     }
 
     const profilePromises = usernames.data.map(async (username) => {
       const profileResult = await getProfileByUsername(username);
-
       return {
         username,
-        name: profileResult.profile.name, // Nombre del usuario
-        surname: profileResult.profile.surname, // Apellido del usuario
-        isVerified: profileResult.profile.is_verified, // Verificación del usuario
+        name: profileResult.profile.name,
+        surname: profileResult.profile.surname,
+        isVerified: profileResult.profile.is_verified,
       };
     });
 
@@ -52,26 +53,48 @@ const VerificationView = () => {
       const profilesWithDetails = await Promise.all(profilePromises);
       setProfiles(profilesWithDetails);
       profilesCache = profilesWithDetails;
+      setFilteredProfiles(profilesWithDetails);
     } catch (err) {
       setError('Error al obtener los perfiles');
     }
 
-    setLoading(false); // Ocultar círculo de carga
+    setLoading(false);
   };
 
   useEffect(() => {
     fetchProfiles();
   }, []);
 
-  const openModal = (user) => {
-    setSelectedUser(user);
-    setIsModalOpen(true);
+  const toggleExpand = (username) => {
+    setExpandedUser(expandedUser === username ? null : username);
+  };
+
+  const openModal = (imageSrc) => {
+    setModalImage(imageSrc);
   };
 
   const closeModal = () => {
-    setSelectedUser(null);
-    setIsModalOpen(false);
+    setModalImage(null);
   };
+
+  const handleSearchChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const handleFilterChange = (status) => {
+    setFilterStatus(status);
+  };
+
+  useEffect(() => {
+    const filtered = profiles.filter((profile) => {
+      const isVerifiedMatch = filterStatus ? profile.isVerified.toString() === filterStatus : true;
+      const usernameMatch = profile.username.toLowerCase().includes(searchTerm.toLowerCase());
+
+      return isVerifiedMatch && usernameMatch;
+    });
+
+    setFilteredProfiles(filtered);
+  }, [searchTerm, filterStatus, profiles]);
 
   return (
     <section className="section">
@@ -79,43 +102,97 @@ const VerificationView = () => {
 
       {loading && (
         <div className="loading-container">
-          <div className="loading-circle"></div> {/* Círculo de carga */}
+          <div className="loading-circle"></div>
         </div>
       )}
 
       {!loading && (
         <>
-          <table>
-            <thead>
-              <tr>
-                <th>Nombre de Usuario</th>
-                <th>Apellido</th>
-                <th>Nombre</th>
-                <th>Verificado</th>
-              </tr>
-            </thead>
-            <tbody>
-              {profiles.map((profile) => (
-                <tr key={profile.username}>
-                  <td>{profile.username}</td>
-                  <td>{profile.surname}</td>
-                  <td>{profile.name}</td>
-                  <td>
-                    <StatusIndicator isVerified={profile.isVerified} />
-                    {profile.isVerified ? 'Sí' : 'No'}
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+          <div className="search-bar">
+            <img
+                src="https://www.citypng.com/public/uploads/preview/magnifying-glass-search-white-icon-transparent-png-701751694974238f0vl5bmpat.png"
+                alt="Lupa"
+                className="verification-search-icon-img"
+              />
+            <input
+              type="text"
+              placeholder="Buscar por nombre de usuario..."
+              value={searchTerm}
+              onChange={handleSearchChange}
+            />
+            <div className="verification-select-container">
+              <select
+                className="styled-select"
+                value={filterStatus}
+                onChange={(e) => handleFilterChange(e.target.value)}
+              >
+                <option value="">Todos</option>
+                <option value="true">Validados</option>
+                <option value="false">No validados</option>
+              </select>
+            </div>
+          </div>
 
-          {isModalOpen && selectedUser && (
-            <UserDetailsModal user={selectedUser} onClose={closeModal} />
-          )}
+          <div className="user-list">
+            {filteredProfiles.map((profile) => (
+              <div key={profile.username} className="user-item">
+                <div className="user-summary" onClick={() => toggleExpand(profile.username)}>
+                  <div>
+                    <StatusIndicator isVerified={profile.isVerified} />
+                    <strong>{profile.username}</strong> - Apellido: {profile.surname} - Nombre: {profile.name}
+                  </div>
+                </div>
+                {expandedUser === profile.username && (
+                  <div className="user-details">
+                    <p><strong>Nombre:</strong> {profile.name}</p>
+                    <p><strong>Apellido:</strong> {profile.surname}</p>
+                    <div className="validation-media">
+                      <div onClick={() => openModal("https://cdn4.iconfinder.com/data/icons/car-misc/100/Driver_license-256.png")}>
+                        <img
+                          src="https://cdn4.iconfinder.com/data/icons/car-misc/100/Driver_license-256.png"
+                          alt="ID Anverso"
+                          className="clickable-image"
+                        />
+                        <p>ID (Anverso)</p>
+                      </div>
+                      <div onClick={() => openModal("https://cdn2.iconfinder.com/data/icons/ecommerce-206/33/member-256.png")}>
+                        <img
+                          src="https://cdn2.iconfinder.com/data/icons/ecommerce-206/33/member-256.png"
+                          alt="ID Reverso"
+                          className="clickable-image"
+                        />
+                        <p>ID (Reverso)</p>
+                      </div>
+                      <div onClick={() => openModal("https://img.a.transfermarkt.technology/portrait/big/8198-1694609670.jpg?lm=1")}>
+                        <img
+                          src="https://img.a.transfermarkt.technology/portrait/big/8198-1694609670.jpg?lm=1"
+                          alt="Selfie"
+                          className="clickable-image"
+                        />
+                        <p>Selfie</p>
+                      </div>
+                    </div>
+                    <div className="validation-actions">
+                      <button className="validate-btn">Validar</button>
+                      <button className="reject-btn">Rechazar</button>
+                    </div>
+                  </div>
+                )}
+              </div>
+            ))}
+          </div>
         </>
       )}
 
       {error && <p>{error}</p>}
+
+      {modalImage && (
+        <div className="modal" onClick={closeModal}>
+          <div className="modal-content">
+            <img src={modalImage} alt="Vista completa" />
+          </div>
+        </div>
+      )}
     </section>
   );
 };
